7a131723efc083ed5b4e2cc4cfbd887aa0a98317
petr <petan.bednar@gmail.com>
2020-10-23 19:39:55 +0200

    add preprocessing text

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,7 @@
 import re
 import glob
 
-#selected dataset - Reuters newsletter
+# selected dataset - Reuters newsletter
 
 START_TEXT = "<TEXT>"
 END_TEXT = "</TEXT>"
@@ -19,8 +19,17 @@ def extract_text(string):
     for item in find_all_text:
         title = re.findall(f'{START_TITLE}(.*?){END_TITLE}', item)
         body = re.findall(f'{START_BODY}(.*?){END_BODY}', item)
-        list_texts.append([title, body])
+        list_texts.append([text_preprocessing(title), text_preprocessing(body)])
     return list_texts
+
+
+def text_preprocessing(string):
+    string = string[0].lower()
+    list_to_drop = ['&lt;', ">", '&#3;', '\\','"']
+
+    for i in list_to_drop:
+        string = string.replace(i, '')
+    return string
 
 
 def load_file(filename):
@@ -46,3 +55,4 @@ if __name__ == '__main__':
 
     with open("output.txt", "w") as file:
         file.write(str(list_of_texts))
+
